e7a4e567b20ef9fe653bf9955194b189aa7204d9
wjddn0719 <jungwoo07191@naver.com>
2025-11-04 06:47:31 +0000

    raspberrypi code wrote

---
+++ raspberry/uart_receive_only.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string.h>
+
+#define UART_PATH = "/dev/serial0";
+#define BAUDRATE = B9600;
+
+int main(){
+    int uart_fd;
+    struct termios options;
+    char buffer[256];
+
+    uart_fd = open(UART_PATH, O_RDWR | O_NOCTTY | O_NDELAY);
+
+    if (uart_fd == -1){
+	perror("Failed to open UART device");
+	
+
+}
